f61ab428d2a4ee9244d1cb250ce4eb079818fa9c
Lucas <lucasscribe2@gmail.com>
2020-11-21 17:02:45 +0100

    🚑 Remove mongodb uri
    Remove mongodb uri

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -5,13 +5,12 @@ export const databaseProvider = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (configService: ConfigService): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb+srv://lucas:xl2UoAyO476QiM8M@lucas-cluster.vwoyi.mongodb.net/genshin-impact?retryWrites=true&w=majority')
-      // mongoose.connect(
-      //   `mongodb+srv://
-      //   ${configService.get('DB_USERNAME')}:
-      //   ${configService.get('DB_PASSWORD')}@
-      //   ${configService.get('DB_HOST')}/
-      //   ${configService.get('DB_NAME')}`,
-      // ),
+      await mongoose.connect(
+        `mongodb+srv://
+        ${configService.get('DB_USERNAME')}:
+        ${configService.get('DB_PASSWORD')}@
+        ${configService.get('DB_HOST')}/
+        ${configService.get('DB_NAME')}`,
+      ),
   },
 ];
